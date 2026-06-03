596747ee97202878c32f4857deeb3ae4b071613d
hansung <hansung@hansungui-Mac.local>
2026-06-03 14:53:37 +0900

    상세화면 상태 보이게함

--- SwipeWordStudy/WordDetailViewController.swift
+++ SwipeWordStudy/WordDetailViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 final class WordDetailViewController: UIViewController {
     @IBOutlet weak var cardView: UIView!
     @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var stateDescriptionLabel: UILabel!
     @IBOutlet weak var termLabel: UILabel!
     @IBOutlet weak var meaningLabel: UILabel!
     @IBOutlet weak var readingLabel: UILabel!
@@ -36,23 +37,29 @@ final class WordDetailViewController: UIViewController {
     private func fillWord() {
         guard let word = word else { return }
 
-        stateLabel.text = word.state.rawValue
+        stateLabel.text = "현재 상태: \(word.state.rawValue)"
         termLabel.text = word.term
         meaningLabel.text = "뜻\n\(word.meaning)"
         readingLabel.text = "읽기\n\(word.reading)"
         exampleLabel.text = "예문\n\(word.example)"
 
         let color: UIColor
+        let stateDescription: String
         switch word.state {
         case .new:
             color = UIFactory.mutedTextColor
+            stateDescription = "아직 학습 전 단어"
         case .memorized:
             color = UIFactory.secondaryColor
+            stateDescription = "외운 단어로 표시됨"
         case .review:
             color = UIFactory.warningColor
+            stateDescription = "다시 봐야 할 단어"
         }
 
         stateLabel.textColor = color
         stateLabel.backgroundColor = color.withAlphaComponent(0.12)
+        stateDescriptionLabel.text = stateDescription
+        stateDescriptionLabel.textColor = color
     }
 }
